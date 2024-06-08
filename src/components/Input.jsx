c6fb4eddc70cd62e4d6a80dbf9787e6bc8cc2bba
chanjin1998 <chanjin@kookmin.ac.kr>
2024-06-08 22:10:11 +0900

    #3 feat: Input 컴포넌트에 placeholder, p태그 추가

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-const Input = ({ children, className, type, onChange }) => {
+const Input = ({ children, className, type, onChange, placeholder }) => {
   return (
-    <input
-      onChange={onChange}
-      className={`bg-white w-[90vw] h-[4vh] rounded-[1vh] border border-gray-300 px-[3vw] py-[2.5vh] ${className}`}
-      type={type}
-    ></input>
+    <div className="flex flex-col items-start">
+      <p className="mb-[1vh]">{children}</p>
+      <input
+        onChange={onChange}
+        placeholder={placeholder}
+        className={`bg-white w-[90vw] h-[4vh] rounded-[1vh] border border-gray-300 px-[3vw] py-[2.5vh] ${className}`}
+        type={type}
+      ></input>
+    </div>
   );
 };
 
