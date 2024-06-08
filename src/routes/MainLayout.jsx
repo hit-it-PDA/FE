7fec7769011683a1ec0966a0bb30cf2ea16c6c73
yeri <rlafl7942@naver.com>
2024-06-08 20:06:21 +0900

    feat: mainLayout에 상단 바 추가

--- src/routes/MainLayout.jsx
+++ src/routes/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import BottomTab from "../components/common/bottomTab/BottomTab";
+import TopBar from "../components/common/topBar/TopBar";
 
 export default function MainLayout() {
   return (
     <div className="w-screen h-screen">
+      <TopBar />
       <Outlet />
       <BottomTab />
     </div>
