fdec59f095437754296a3b50703a17ba09d98358
yeri <rlafl7942@naver.com>
2024-06-08 20:02:11 +0900

    feat: 알림 페이지 생성 후 상단 바 라우터 연결

--- src/components/common/topBar/TopBar.jsx
+++ src/components/common/topBar/TopBar.jsx
@@ -5,12 +5,22 @@ import logo from "../../../assets/logo.svg";
 
 // icons
 import noti from "../../../assets/icons/notification.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function TopBar() {
+  const navigate = useNavigate();
   return (
     <div className="m-3 flex flex-row items-center justify-between h-[6vh]">
-      <img src={logo} className="w-[12vw]" />
-      <img src={noti} className="w-[11vw]" />
+      <img
+        src={logo}
+        className="w-[12vw] hover:cursor-pointer"
+        onClick={() => navigate("/")}
+      />
+      <img
+        src={noti}
+        className="w-[11vw] hover:cursor-pointer"
+        onClick={() => navigate("/notification")}
+      />
     </div>
   );
 }
