1c4b9d6e32e8264adc6dcd591d026d3ca846812a
yeri <rlafl7942@naver.com>
2024-06-08 21:30:16 +0900

    #8 feat: Home 라우터 수정
    HomeLayout 라우터 연결

--- src/routers/mainRouter.jsx
+++ src/routers/mainRouter.jsx
@@ -2,14 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 
 import MainLayout from "../routes/MainLayout";
 
-// page
+// pages
 import HomePage from "../routes/home/HomePage";
 import PortfolioPage from "../routes/portfolio/PortfolioPage";
 import AssetPage from "../routes/asset/AssetPage";
 import MorePage from "../routes/more/MorePage";
 import LoginPage from "../routes/login/LoginPage";
-import TopBar from "../components/common/topBar/TopBar";
 import NotificationPage from "../routes/notification/NotificationPage";
+
+// layouts
+import HomeLayout from "../routes/home/HomeLayout";
 
 export const mainRouter = [
   {
@@ -17,9 +19,15 @@ export const mainRouter = [
     element: <MainLayout />,
     children: [
       {
-        path: "",
-        element: <HomePage />,
-        index: true,
+        path: "/home",
+        element: <HomeLayout />,
+        children: [
+          {
+            path: "",
+            element: <HomePage />,
+            index: true,
+          },
+        ],
       },
       {
         path: "/portfolio",
