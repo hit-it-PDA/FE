78474428b61eacab33464b4d2858713ca8cf7c13
yeri <rlafl7942@naver.com>
2024-06-09 19:21:27 +0900

    #8 feat: Setting 레이아웃 및 페이지 생성

---
+++ src/routes/setting/SettingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TopBar from "../../components/common/topBar/TopBar";
+
+export default function SettingPage() {
+  return (
+    <div>
+      <TopBar type={2} />
+      SettingPage
+    </div>
+  );
+}
