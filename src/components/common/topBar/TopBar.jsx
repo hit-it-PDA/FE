a612480002e3c223a708c9de95b0d5295c7df26c
yeri <rlafl7942@naver.com>
2024-06-09 18:23:59 +0900

    #8 feat: TopBar Component 분리 및 renderTopBar 추가

--- src/components/common/topBar/TopBar.jsx
+++ src/components/common/topBar/TopBar.jsx
@@ -1,26 +1,27 @@
 import React from "react";
-
-// logo
-import logo from "../../../assets/logo.svg";
-
-// icons
-import noti from "../../../assets/icons/notification.svg";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import {
+  BackArrowWithNotification,
+  LogoWithNotification,
+  NotificationWithSetting,
+} from "./TopBarComponents";
 
 export default function TopBar() {
-  const navigate = useNavigate();
-  return (
-    <div className="p-3 flex flex-row items-center justify-between h-[7vh] bg-white">
-      <img
-        src={logo}
-        className="w-[10vw] max-w-10 hover:cursor-pointer"
-        onClick={() => navigate("/")}
-      />
-      <img
-        src={noti}
-        className="w-[10vw] max-w-10 hover:cursor-pointer"
-        onClick={() => navigate("/notification")}
-      />
-    </div>
-  );
+  const location = useLocation();
+
+  const path = location.pathname;
+
+  const renderTopBar = () => {
+    switch (path) {
+      case "/home":
+      case "/asset":
+      case "/manage":
+        return <LogoWithNotification />;
+      case "/more":
+        return <NotificationWithSetting />;
+      default:
+        return <BackArrowWithNotification />;
+    }
+  };
+  return <>{renderTopBar()}</>;
 }
