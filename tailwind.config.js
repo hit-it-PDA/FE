d8ee189f6dce2ea24f2bcc41aa96c111ab74dde2
chanjin1998 <chanjin@kookmin.ac.kr>
2024-06-09 23:24:34 +0900

    #10 chore: tailwind plugins accent-color 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,23 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      width: {
-        
-      },
+      width: {},
       colors: {
         main_yellow: "#FFDE71",
         input_color: "#D9D9D9",
+        progress_color: "#F3F3F3",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".accent-customYellow": {
+          accentColor: "#FFDE71",
+        },
+      };
+
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
 };
