69e9cc3e1f1ae0b12bf450b31ad3dd774d0d45f5
yeri <rlafl7942@naver.com>
2024-06-09 20:42:28 +0900

    #8 feat: 화면 url에 따라 하단 바 상태 변경

--- src/components/common/bottomTab/BottomTab.jsx
+++ src/components/common/bottomTab/BottomTab.jsx
@@ -9,7 +9,16 @@ import asset_selected from "../../../assets/icons/asset_selected.svg";
 import manage_selected from "../../../assets/icons/manage_selected.svg";
 import home_selected from "../../../assets/icons/home_selected.svg";
 import more_selected from "../../../assets/icons/more_selected.svg";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useCallback } from "react";
+
+const TABS = {
+  홈: "",
+  노후: "manage",
+  자산: "asset",
+  더보기: "more",
+};
 
 export default function BottomTab() {
   const [isSelected, setSelected] = useState("홈");
@@ -49,15 +58,22 @@ export default function BottomTab() {
 
 const Tab = ({ img, imgSelected, title, setSelected, isSelected }) => {
   const navigate = useNavigate();
+  const location = useLocation();
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setSelected("홈");
+    } else if (location.pathname.includes("more")) {
+      setSelected("더보기");
+    } else if (location.pathname.includes("asset")) {
+      setSelected("자산");
+    } else if (location.pathname.includes("manage")) {
+      setSelected("노후");
+    }
+  }, [location.pathname, setSelected]);
+
   const onClickFn = () => {
-    const tabs = {
-      홈: "",
-      노후: "manage",
-      자산: "asset",
-      더보기: "more",
-    };
-    setSelected(title);
-    navigate(`/${tabs[title]}`);
+    if (isSelected) return;
+    navigate(`/${TABS[title]}`);
   };
   return (
     <div
