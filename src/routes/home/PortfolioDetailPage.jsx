5492652767ab36e3654954d46f6c65c9e54e9e08
yeri <rlafl7942@naver.com>
2024-06-10 16:44:29 +0900

    #14 feat: 상품 비중 UI 구현

--- src/routes/home/PortfolioDetailPage.jsx
+++ src/routes/home/PortfolioDetailPage.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+
+// components
+import PortfolioRatioComponent from "../../components/common/home/PortfolioRatioComponent";
 import RecommendComponent from "../../components/common/home/RecommendComponent";
 import TopBar from "../../components/common/topBar/TopBar";
 
@@ -7,6 +10,7 @@ export default function PortfolioDetailPage() {
     <div className="bg-white">
       <TopBar type={3} />
       <RecommendComponent type={1} />
+      <PortfolioRatioComponent stockRatio={17} bondRatio={56} />
     </div>
   );
 }
