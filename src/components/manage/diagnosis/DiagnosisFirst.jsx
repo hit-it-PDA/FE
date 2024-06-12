36c7a11d37fbafdf02be40f5b7f4248e9726a1a2
yeri <rlafl7942@naver.com>
2024-06-12 17:30:18 +0900

    #28 feat: 첫 번째 페이지 UI 구현

---
+++ src/components/manage/diagnosis/DiagnosisFirst.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+// components
+import Input from "../../Input";
+
+export default function DiagnosisFirst() {
+  const [sex, setSex] = useState(0);
+  return (
+    <div>
+      <span className="text-[20px] font-bold">기본 사항</span>
+      <div className="mt-5 gap-3 flex flex-col">
+        <Input placeholder={"ex) 만 26세"}>만 나이를 입력하세요.</Input>
+        <div>
+          <p className="mb-[1vh]">성별을 입력하세요.</p>
+          <div className="flex justify-between items-center">
+            <Button
+              className={`w-[43vw] bg-${
+                sex === 1 ? "main_yellow" : "[#f6f6f6]"
+              }`}
+              setSex={setSex}
+            >
+              남성
+            </Button>
+            <Button
+              className={`w-[43vw] bg-${
+                sex === 2 ? "main_yellow" : "[#f6f6f6]"
+              }`}
+              setSex={setSex}
+            >
+              여성
+            </Button>
+          </div>
+        </div>
+        <Input placeholder={"ex) 55세"}>희망 퇴직 나이를 입력하세요.</Input>
+        <Input placeholder={"ex) 103만원"}>
+          국민연금 예상 수령액을 입력하세요.
+        </Input>
+        <span className="text-[13px] text-[#505050] whitespace-pre-line">
+          {`(국민연금공단, 2023년 1월 기준) 
+            국민연금 20년 이상 가입자의 월 평균 수령액은 103만원입니다.
+            본인이 확인한 예상 수령 금액을 입력해주세요.`}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+const Button = ({ children, className, setSex }) => {
+  return (
+    <button
+      className={`h-[5vh] rounded-[10px] ${className}`}
+      onClick={() => setSex(children === "남성" ? 1 : 2)}
+    >
+      {children}
+    </button>
+  );
+};
--- src/routes/manage/ManageDiagnosisPage.jsx
+++ src/routes/manage/ManageDiagnosisPage.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
+// components
+import TopBar from "../../components/common/topBar/TopBar";
+import DiagnosisFirst from "../../components/manage/diagnosis/DiagnosisFirst";
 
 export default function ManageDiagnosisPage() {
-  return <div>ManageDiagnosisPage</div>;
+  const [seq, setSeq] = useState(0);
+  return (
+    <div>
+      <TopBar type={2} />
+      <div className="w-full h-[80vh] px-5">
+        <div className="flex flex-col w-full h-full justify-between">
+          {renderDiagnosis(seq)}
+          <button
+            className="w-[30vw] self-end bg-main_yellow px-5 py-3 rounded-[10px]"
+            onClick={() => setSeq((prev) => prev + 1)}
+          >
+            다음
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+const renderDiagnosis = (seq) => {
+  switch (seq) {
+    case 0:
+      return <DiagnosisFirst />;
+
+    default:
+      break;
+  }
+};
