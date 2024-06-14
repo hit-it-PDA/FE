093d4f616040f83e894a76bfa16c3febcc41c40b
yeri <rlafl7942@naver.com>
2024-06-14 11:38:51 +0900

    #28 feat: 버튼 컴포넌트화

---
+++ src/components/manage/diagnosis/ButtonComponent.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function ButtonComponent({ question, state, func, className }) {
+  return (
+    <div className={className}>
+      <p className="mb-[1vh]">{question}</p>
+      <div className="flex justify-between items-center">
+        <Button
+          className={`bg-${state === 1 ? "main_yellow" : "[#f6f6f6]"}`}
+          func={func}
+        >
+          예
+        </Button>
+        <Button
+          className={`bg-${state === 2 ? "main_yellow" : "[#f6f6f6]"}`}
+          func={func}
+        >
+          아니오
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+const Button = ({ children, className, func }) => {
+  return (
+    <button
+      className={`w-[43vw] h-[5vh] rounded-[10px] ${className}`}
+      onClick={() => {
+        func(children === "예" ? 1 : 2);
+      }}
+    >
+      {children}
+    </button>
+  );
+};
