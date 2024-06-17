b783a732010535195bfe5dcfaeb6dac549deaee7
chanjin1998 <chanjin@kookmin.ac.kr>
2024-06-17 19:31:07 +0900

    #41 fix: console.log 제거

--- src/lib/apis/userApi.jsx
+++ src/lib/apis/userApi.jsx
@@ -12,7 +12,6 @@ async function login(reqBody) {
   try {
     const response = await instance.post(`/users/login`, reqBody);
     if (response.status === 200) {
-      console.log(response.data.response.token);
       const token = response.data.response.token; // 서버 응답에서 토큰을 추출
       localStorage.setItem("accessToken", token); // 로컬 스토리지에 토큰 저장
     }
