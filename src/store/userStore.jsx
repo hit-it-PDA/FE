39991bb9fdf7fb84b7a7ce3b064982b1bee16cb3
chanjin1998 <chanjin@kookmin.ac.kr>
2024-06-18 09:37:57 +0900

    #43 feat: zustand store user정보 localStorge에 persist 처리

--- src/store/userStore.jsx
+++ src/store/userStore.jsx
@@ -1,9 +1,16 @@
-import {create} from 'zustand';
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useUserStore = create((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
-  clearUser: () => set({ user: null }),
-}));
-
+const useUserStore = create(
+  persist(
+    (set) => ({
+      user: { name: "방문자", type: "미정" },
+      setUser: (user) => set({ user }),
+      clearUser: () => set({ user: null }),
+    }),
+    {
+      name: "user-storage",
+    }
+  )
+);
 export default useUserStore;
