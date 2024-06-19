f55a24ae9b1be6f7436d4c233ef6238b9d90c1bc
chanjin1998 <chanjin@kookmin.ac.kr>
2024-06-19 15:23:30 +0900

    #53

--- src/routes/investTest/InvestTestResultPage.jsx
+++ src/routes/investTest/InvestTestResultPage.jsx
@@ -7,15 +7,32 @@ import InvestResultRed from "../../components/common/investResult/InvestResultRe
 import Button from "../../components/Button";
 import { useEffect } from "react";
 import RobotAnalyzing from "../../components/home/RobotAnalyzing";
+import { postTestResult } from "../../lib/apis/testApi";
+import useUserStore from "../../store/userStore";
 
 export default function InvestTestResultPage() {
   const [isLoading, setIsLoading] = useState(true);
+  const [result, setResult] = useState("");
   const location = useLocation();
-  const testDatas = location.state.testDatas;
+  const { modifyType } = useUserStore();
+
+  const testDatas = location.state.resultDatas;
   const score = location.state.score;
+  const postDatas = location.state.testDatas;
   console.log(score);
   console.log(testDatas);
+  console.log(postDatas);
 
+  const fetchTestResult = async () => {
+    try {
+      const response = await postTestResult(postDatas);
+      setResult(response.data.response);
+      modifyType(response.data.response);
+      console.log(response.data.response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
     const intervalId = setInterval(() => {
       setIsLoading(false);
@@ -24,16 +41,32 @@ export default function InvestTestResultPage() {
     return () => clearInterval(intervalId);
   }, []);
 
+  useEffect(() => {
+    fetchTestResult();
+  }, []);
+
   const renderPage = () => {
-    if (score < 40) {
-      return <InvestResultGreen testDatas={testDatas} />;
-    } else if (score >= 40 && score < 80) {
-      return <InvestResultBlue testDatas={testDatas} />;
-    } else if (score >= 80 && score < 120) {
-      return <InvestResultOrange testDatas={testDatas} />;
-    } else {
-      return <InvestResultRed testDatas={testDatas} />;
+    switch (result) {
+      case "안정형":
+        return <InvestResultGreen testDatas={testDatas} />;
+      case "안정추구형":
+        return <InvestResultBlue testDatas={testDatas} />;
+      case "위험중립형":
+        return <InvestResultBlue testDatas={testDatas} />;
+      case "적극투자형":
+        return <InvestResultOrange testDatas={testDatas} />;
+      case "공격투자형":
+        return <InvestResultRed testDatas={testDatas} />;
     }
+    // if (score < 40) {
+    //   return <InvestResultGreen testDatas={testDatas} />;
+    // } else if (score >= 40 && score < 80) {
+    //   return <InvestResultBlue testDatas={testDatas} />;
+    // } else if (score >= 80 && score < 120) {
+    //   return <InvestResultOrange testDatas={testDatas} />;
+    // } else {
+    //   return <InvestResultRed testDatas={testDatas} />;
+    // }
   };
   return (
     <div>
