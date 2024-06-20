ed67691e25bcb2c2044bf0e8aa244c23bdd8fc9f
yeri <rlafl7942@naver.com>
2024-06-20 15:02:48 +0900

    #54 feat: 포트폴리오 구성 컴포넌트 추가

--- src/components/home/PortfolioCompositionComponent.jsx
+++ src/components/home/PortfolioCompositionComponent.jsx
@@ -1,25 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 // components
 import DoughnutChartComponent from "../common/chart/DoughnutChartComponent";
+import FundChartComponent from "../common/chart/FundChartComponent";
 
-export default function PortfolioCompositionComponent({
-  title,
-  price,
-  type,
-  ratio,
-}) {
+export default function PortfolioCompositionComponent({ type, data }) {
+  const [weight, setWeight] = useState([]);
+  const STOCK_COLORS = [
+    "#EBEEFF",
+    "#D8DDFF",
+    "#C4CCFF",
+    "#B0BBFF",
+    "#9CAAFF",
+    "#8899FF",
+    "#7488FF",
+    "#6077FF",
+    "#4B66FF",
+    "#3755FF",
+  ];
+  const BOND_COLORS = [
+    "#FFE7E7",
+    "#FFD9D9",
+    "#FFCCCC",
+    "#FFC1C1",
+    "#FFB2B2",
+    "#FFA3A3",
+    "#FF8F8F",
+    "#FF8585",
+    "#FF7676",
+    "#FE6969",
+  ];
+  useEffect(() => {
+    setWeight(data.map((elem) => elem.weight));
+  }, [data]);
   return (
-    <div
-      className={`${
-        type === "stock" ? "bg-sub" : "bg-[#FFDFD5]"
-      } flex items-center justify-between rounded-[25px] w-full h-[13vh] px-5 py-3`}
-    >
-      <div className="flex flex-col">
-        <span className="font-bold">{title}</span>
-        <span>{price}원</span>
+    <>
+      <div className="flex flex-col items-center gap-3 mt-3">
+        {data.length > 0 ? (
+          <>
+            <FundChartComponent type={type} ratio={weight} />
+            {data.map((elem, index) => (
+              <div
+                key={index}
+                className="flex w-full h-[5vh] rounded-[30px]"
+                style={{
+                  backgroundColor: `${
+                    type === "stock" ? STOCK_COLORS[index] : BOND_COLORS[index]
+                  }`,
+                }}
+              >
+                <div className="flex items-center justify-between w-full px-5">
+                  <span className="flex-1 truncate mr-1 font-bold text-[15px]">
+                    {elem.stock_name || elem.bond_name}
+                  </span>
+                  <span className="text-[13px]">{elem.weight}%</span>
+                </div>
+              </div>
+            ))}
+          </>
+        ) : (
+          <div className="flex justify-center w-full">데이터가 없습니다.</div>
+        )}
       </div>
-      <DoughnutChartComponent type={type} ratio={ratio} />
-    </div>
+    </>
   );
 }
