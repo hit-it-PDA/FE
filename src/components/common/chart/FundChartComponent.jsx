35b3fbfc90db22cabf76991783693650474a998a
yeri <rlafl7942@naver.com>
2024-06-20 15:02:34 +0900

    #54 feat: 주식, 채권 색상 추가 및 type에 따른 색상 변경

--- src/components/common/chart/FundChartComponent.jsx
+++ src/components/common/chart/FundChartComponent.jsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Chart as ChartJS, ArcElement, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Legend);
 
 export default function FundChartComponent({ type, ratio, className }) {
+  // const defaultColors = [
+  //   "#F0F4FF",
+  //   "#E4ECFF",
+  //   "#CCD8FF",
+  //   "#B3C5FF",
+  //   "#98B1FF",
+  //   "#6A8CFF",
+  //   "#375AFF",
+  //   "#B3C5FF",
+  //   "#B3C5FF",
+  //   "#B3C5FF",
+  // ]; // 기본 색상 배열
   const defaultColors = [
-    "#FFFFFF",
-    "#F0F4FF",
-    "#E4ECFF",
-    "#CCD8FF",
-    "#B3C5FF",
-    "#98B1FF",
-    "#6A8CFF",
-  ]; // 기본 색상 배열
+    "#EBEEFF",
+    "#D8DDFF",
+    "#C4CCFF",
+    "#B0BBFF",
+    "#9CAAFF",
+    "#8899FF",
+    "#7488FF",
+    "#6077FF",
+    "#4B66FF",
+    "#3755FF",
+  ];
+  const BOND_COLORS = [
+    "#FFE7E7",
+    "#FFD9D9",
+    "#FFCCCC",
+    "#FFC1C1",
+    "#FFB2B2",
+    "#FFA3A3",
+    "#FF8F8F",
+    "#FF8585",
+    "#FF7676",
+  ];
   const backgroundColors = ratio.map(
     (_, index) => defaultColors[index % defaultColors.length]
   );
@@ -24,7 +50,7 @@ export default function FundChartComponent({ type, ratio, className }) {
       {
         label: "test",
         data: [...ratio, 100 - ratio.reduce((a, b) => a + b, 0)],
-        backgroundColor: backgroundColors,
+        backgroundColor: type === "stock" ? backgroundColors : BOND_COLORS,
         borderWidth: 0.8,
       },
     ],
@@ -42,7 +68,6 @@ export default function FundChartComponent({ type, ratio, className }) {
       },
     },
   };
-
   return (
     <>
       {className ? (
