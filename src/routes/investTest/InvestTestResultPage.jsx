fae063d0e170f6f548afc4c7990b3ba4cb429f65
chanjin1998 <chanjin@kookmin.ac.kr>
2024-06-20 14:48:59 +0900

    #56 feat: 투자성향테스트 결과 저장하는 배열 변경

--- src/routes/investTest/InvestTestResultPage.jsx
+++ src/routes/investTest/InvestTestResultPage.jsx
@@ -4,31 +4,37 @@ import InvestResultBlue from "../../components/common/investResult/InvestResultB
 import InvestResultGreen from "../../components/common/investResult/InvestResultGreen";
 import InvestResultOrange from "../../components/common/investResult/InvestResultOrange";
 import InvestResultRed from "../../components/common/investResult/InvestResultRed";
+import InvestResultYellow from "../../components/common/investResult/InvestResultYellow";
 import Button from "../../components/Button";
 import { useEffect } from "react";
 import RobotAnalyzing from "../../components/home/RobotAnalyzing";
-import { postTestResult } from "../../lib/apis/testApi";
+import { postTestResult, getTestResult } from "../../lib/apis/testApi";
 import useUserStore from "../../store/userStore";
 
 export default function InvestTestResultPage() {
   const [isLoading, setIsLoading] = useState(true);
+  const [isLogin, setIsLogin] = useState("");
   const [result, setResult] = useState("");
+  const [testDatas, setTestDatas] = useState([]);
   const location = useLocation();
   const { modifyType } = useUserStore();
-
-  const testDatas = location.state.resultDatas;
-  const score = location.state.score;
+  const user = useUserStore((state) => state.user);
+  const token = localStorage.getItem("accessToken");
   const postDatas = location.state.testDatas;
-  console.log(score);
-  console.log(testDatas);
-  console.log(postDatas);
 
   const fetchTestResult = async () => {
     try {
       const response = await postTestResult(postDatas);
       setResult(response.data.response);
       modifyType(response.data.response);
-      console.log(response.data.response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const fetchGetTestResult = async () => {
+    try {
+      const response = await getTestResult();
+      setTestDatas(response.data.response);
     } catch (error) {
       console.log(error);
     }
@@ -36,13 +42,15 @@ export default function InvestTestResultPage() {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setIsLoading(false);
-    }, 1500);
+    }, 2500);
 
     return () => clearInterval(intervalId);
   }, []);
 
   useEffect(() => {
     fetchTestResult();
+    fetchGetTestResult();
+    token ? setIsLogin(token) : setIsLogin("");
   }, []);
 
   const renderPage = () => {
@@ -52,27 +60,18 @@ export default function InvestTestResultPage() {
       case "안정추구형":
         return <InvestResultBlue testDatas={testDatas} />;
       case "위험중립형":
-        return <InvestResultBlue testDatas={testDatas} />;
+        return <InvestResultYellow testDatas={testDatas} />;
       case "적극투자형":
         return <InvestResultOrange testDatas={testDatas} />;
       case "공격투자형":
         return <InvestResultRed testDatas={testDatas} />;
     }
-    // if (score < 40) {
-    //   return <InvestResultGreen testDatas={testDatas} />;
-    // } else if (score >= 40 && score < 80) {
-    //   return <InvestResultBlue testDatas={testDatas} />;
-    // } else if (score >= 80 && score < 120) {
-    //   return <InvestResultOrange testDatas={testDatas} />;
-    // } else {
-    //   return <InvestResultRed testDatas={testDatas} />;
-    // }
   };
   return (
     <div>
       {isLoading ? (
         <div className="w-full h-[80vh] flex items-center justify-center">
-          <RobotAnalyzing>000님의 투자 성향을 분석 중</RobotAnalyzing>
+          <RobotAnalyzing>{user.name}님의 투자 성향을 분석 중</RobotAnalyzing>
         </div>
       ) : (
         renderPage()
