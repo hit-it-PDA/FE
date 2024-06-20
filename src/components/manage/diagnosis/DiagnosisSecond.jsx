b995451992ededbdc49444bd4ed535e8543df2a4
yeri <rlafl7942@naver.com>
2024-06-21 08:25:42 +0900

    #62 feat: 진단 두번째 페이지 문항 수정

--- src/components/manage/diagnosis/DiagnosisSecond.jsx
+++ src/components/manage/diagnosis/DiagnosisSecond.jsx
@@ -1,27 +1,59 @@
 import React from "react";
+import { useState } from "react";
 import Input from "../../Input";
+import SelectComponent from "./SelectComponent";
 
-export default function DiagnosisSecond({
-  incomeAmount,
-  setIncomeAmount,
-  returnExp,
-  setReturnExp,
-  livingExpenses,
-  setLivingExpenses,
-  manageScore,
-  setManageScore,
-}) {
+export default function DiagnosisSecond() {
+  const [asset, setAsset] = useState();
+  const [returns, setReturns] = useState();
+  const assetOptions = [
+    {
+      label: "3억",
+      value: "3억",
+    },
+    {
+      label: "5억",
+      value: "5억",
+    },
+  ];
+  const returnsOptions = [
+    {
+      label: "1%",
+      value: "1",
+    },
+    {
+      label: "5%",
+      value: "5",
+    },
+  ];
   return (
     <div>
-      <span className="text-[25px] font-bold">기본 사항</span>
-      <div className="mt-5 gap-3 flex flex-col">
-        <Input placeholder={"ex) 100만원"}>금융 소득을 입력하세요.</Input>
-        <Input placeholder={"ex) 3%"}>예상 투자 수익률을 입력하세요.</Input>
-        <Input placeholder={"ex) 200만원"}>월 생활비를 입력하세요.</Input>
-        <Input placeholder={"0~10 사이의 숫자를 입력해주세요."}>
-          나의 재무·건강·여가활동·대인관계에 대한 노후준비는 몇점인가요?
-          <br />
+      <span className="text-[25px] font-bold">재무 준비 실천 행위</span>
+      <div className="flex flex-col gap-3 mt-5">
+        <Input placeholder={"ex) 103만원"}>
+          국민연금 예상 수령액을 입력해주세요.
         </Input>
+        <span className="text-[13px] text-[#505050] whitespace-pre-line">
+          {`국민연금 20년 이상 가입자의 월 평균 수령액은 103만원입니다. (국민연금공단, 2023년 1월 기준) 
+            본인이 확인한 예상 수령 금액을 입력해주세요.`}
+        </span>
+        <Input placeholder={"ex) 1000만원"}>
+          월세가 나오거나 노후에 주택연금 등을 이용해 생활비로 쓸 부동산
+          금액(시세) 총액을 입력해주세요.
+        </Input>
+        <span className="text-[25px] font-bold mt-3">재무 상태</span>
+        <SelectComponent
+          question={"금융자산 총액을 입력해주세요. (부동산을 제외한 예금 등)"}
+          options={assetOptions}
+          state={asset}
+          func={setAsset}
+        />
+        <SelectComponent
+          question={"예상 투자 수익률을 입력해주세요."}
+          options={returnsOptions}
+          state={returns}
+          func={setReturns}
+        />
       </div>
     </div>
   );
