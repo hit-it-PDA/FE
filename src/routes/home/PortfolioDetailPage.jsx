54eb9145f2ddf2c38eaeed6903059bef20d36d1e
yeri <rlafl7942@naver.com>
2024-06-20 14:38:23 +0900

    #54 feat: 펀드 제목 괄호 기준으로 split 추가

--- src/routes/home/PortfolioDetailPage.jsx
+++ src/routes/home/PortfolioDetailPage.jsx
@@ -2,7 +2,7 @@ import { data } from "autoprefixer";
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 // components
 import FundChartComponent from "../../components/common/chart/FundChartComponent";
@@ -18,7 +18,7 @@ export default function PortfolioDetailPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [fundListData, setFundListData] = useState([]);
   const [fundRatio, setFundRatio] = useState([]);
-  const { id: params_id } = useParams();
+  const { portfolioId: params_id } = useParams();
   const getData = async () => {
     const data = await getFundList(params_id);
     setFundListData(data.response);
@@ -27,18 +27,21 @@ export default function PortfolioDetailPage() {
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [params_id]);
 
   return (
     <div>
       <TopBar type={3} />
       <div className="w-[88vw] mx-auto flex flex-col items-center">
-        <span className="w-[88vw] text-[23px] font-bold">📌 펀드 구성</span>
+        <span className="w-[88vw] text-[23px] font-bold">
+          📌 포트폴리오 구성
+        </span>
         <FundChartComponent
+          type="stock"
           ratio={fundRatio}
           className={"w-[60vw] h-[30vh] mt-[2vh]"}
         />
-        <div className="flex flex-col gap-7 mt-10">
+        <div className="flex flex-col mt-10 gap-7">
           {fundListData.map((elem, index) => (
             <FundListComponent key={index} index={index} data={elem} />
           ))}
@@ -49,6 +52,7 @@ export default function PortfolioDetailPage() {
 }
 
 const FundListComponent = ({ index, data }) => {
+  const navigate = useNavigate();
   const COLORS = [
     "#F0F4FF",
     "#E4ECFF",
@@ -58,10 +62,14 @@ const FundListComponent = ({ index, data }) => {
     "#6A8CFF",
     "#375AFF",
   ];
-  const splitFundName = data.fundName.split(/([(\[])/);
+  let splitFundName = "";
+  if (data.fundName.includes("[") || data.fundName.includes("(")) {
+    splitFundName = data.fundName.split(/([(\[])/);
+  }
   return (
     <div
       className={`flex justify-between w-[90vw] rounded-[25px] px-5 py-5 shadow-lg hover:cursor-pointer`}
+      onClick={() => navigate(`${index}`)}
       style={{ backgroundColor: `${COLORS[index]}` }}
     >
       <div className="flex flex-col flex-1 mr-2 truncate">
@@ -69,8 +77,8 @@ const FundListComponent = ({ index, data }) => {
           <span className="text-[17px] font-bold">{splitFundName[0]}</span>
         </div>
         <div className="w-full flex truncate text-[15px]">
-          {splitFundName[1]}
-          {splitFundName.slice(2).join("")}
+          {splitFundName?.[1]}
+          {splitFundName?.slice(2).join("")}
         </div>
         <span className="text-[13px]">{data.fundTypeDetail}</span>
       </div>
