56e0ec59e9acdc9e97cec46517844eb485de0e45
chanjin1998 <chanjin@kookmin.ac.kr>
2024-06-21 10:27:26 +0900

    #61 feat: 총자산, 은행, 증권, 연금, 카드, 대출 조회 api 추가

--- src/lib/apis/mydataApi.jsx
+++ src/lib/apis/mydataApi.jsx
@@ -13,4 +13,66 @@ async function postMydata(selectedItemsByType) {
   }
 }
 
-export { postMydata };
+async function getAllAssets() {
+  try {
+    const response = await authInstance(8083).get(`/assets/totalAssets`);
+    return response.data;
+  } catch (error) {
+    return error.response;
+  }
+}
+
+async function getBankAssets() {
+  try {
+    const response = await authInstance(8083).get(`/assets/bank-accounts`);
+    return response.data;
+  } catch (error) {
+    return error.response;
+  }
+}
+
+async function getSecurityAssets() {
+  try {
+    const response = await authInstance(8083).get(`/assets/security-accounts`);
+    return response.data;
+  } catch (error) {
+    return error.response;
+  }
+}
+
+async function getCardAssets() {
+  try {
+    const response = await authInstance(8083).get(`/assets/cards`);
+    return response.data;
+  } catch (error) {
+    return error.response;
+  }
+}
+
+async function getLoanAssets() {
+  try {
+    const response = await authInstance(8083).get(`/assets/loans`);
+    return response.data;
+  } catch (error) {
+    return error.response;
+  }
+}
+
+async function getPensionAssets() {
+  try {
+    const response = await authInstance(8083).get(`/assets/pensions`);
+    return response.data;
+  } catch (error) {
+    return error.response;
+  }
+}
+
+export {
+  postMydata,
+  getAllAssets,
+  getBankAssets,
+  getSecurityAssets,
+  getCardAssets,
+  getLoanAssets,
+  getPensionAssets,
+};
