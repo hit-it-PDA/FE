548626a2e5d88b8e9806fde450694fa5d0ea415a
yeri <rlafl7942@naver.com>
2024-06-21 10:51:03 +0900

    #62 feat: input 입력 값 handle을 위한 onBlur 추가

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
-const Input = ({ children, className, type, onChange, placeholder }) => {
+const Input = ({
+  children,
+  className,
+  type,
+  onChange,
+  onBlur,
+  placeholder,
+  value,
+}) => {
   return (
     <div className="flex flex-col items-start">
       <p className="mb-[1vh]">{children}</p>
       <input
         onChange={onChange}
+        onBlur={onBlur}
         placeholder={placeholder}
         className={`bg-white w-[90vw] h-[4vh] rounded-[1vh] border border-gray-300 px-[3vw] py-[2.5vh] ${className}`}
         type={type}
+        value={value}
       ></input>
     </div>
   );
