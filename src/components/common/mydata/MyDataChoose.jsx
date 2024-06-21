95e8b71c4f25f57404e6ba771b2184a7db1fd085
chanjin1998 <chanjin@kookmin.ac.kr>
2024-06-21 10:28:08 +0900

    #61 feat: logo 추가, map 함수 구조 변경

--- src/components/common/mydata/MyDataChoose.jsx
+++ src/components/common/mydata/MyDataChoose.jsx
@@ -1,7 +1,21 @@
-import logo from "../../../assets/logos/app_logo.jpg";
 import React, { useState, useEffect } from "react";
-import Button from "../../Button";
 import { useNavigate } from "react-router-dom";
+// assets
+import shinhan from "../../../assets/companyLogos/shinhan.png";
+import kb from "../../../assets/companyLogos/kb.png";
+import hana from "../../../assets/companyLogos/hana.png";
+import lotte from "../../../assets/companyLogos/lotte.png";
+import nh from "../../../assets/companyLogos/nh.png";
+import woori from "../../../assets/companyLogos/woori.jpeg";
+import dgb from "../../../assets/companyLogos/dgb.jpeg";
+import kakao from "../../../assets/companyLogos/kakao.png";
+import kiwoom from "../../../assets/companyLogos/kiwoom.png";
+import ibk from "../../../assets/companyLogos/ibk.png";
+import korea from "../../../assets/companyLogos/korea.jpeg";
+import mirae from "../../../assets/companyLogos/mirae.png";
+import bc_card from "../../../assets/companyLogos/bc_card1.jpeg";
+// components
+import Button from "../../Button";
 
 export default function MyDataChoose({
   handleButtonClick,
@@ -13,42 +27,60 @@ export default function MyDataChoose({
     {
       name: "은행",
       content: [
-        "신한은행",
-        "NH농협은행",
-        "하나은행",
-        "IBK기업은행",
-        "카카오뱅크",
-        "KB국민은행",
-        "DGB대구은행",
-        "우리은행",
+        { company: "신한은행", logo: shinhan },
+        { company: "NH농협은행", logo: nh },
+        { company: "하나은행", logo: hana },
+        { company: "IBK기업은행", logo: ibk },
+        { company: "카카오뱅크", logo: kakao },
+        { company: "KB국민은행", logo: kb },
+        { company: "DGB대구은행", logo: dgb },
+        { company: "우리은행", logo: woori },
       ],
       type: "bank_accounts",
     },
     {
       name: "증권",
       content: [
-        "신한투자증권",
-        "하나증권",
-        "한국투자증권",
-        "키움증권",
-        "미래에셋대우",
-        "KB증권",
+        { company: "신한투자증권", logo: shinhan },
+        { company: "하나증권", logo: hana },
+        { company: "한국투자증권", logo: korea },
+        { company: "키움증권", logo: kiwoom },
+        { company: "미래에셋대우", logo: mirae },
+        { company: "KB증권", logo: kb },
       ],
       type: "security_accounts",
     },
     {
       name: "카드",
-      content: ["신한카드", "KB국민카드", "BC카드", "하나카드", "롯데카드"],
+      content: [
+        { company: "신한카드", logo: shinhan },
+        { company: "KB국민카드", logo: kb },
+        { company: "BC카드", logo: bc_card },
+        { company: "하나카드", logo: hana },
+        { company: "롯데카드", logo: lotte },
+      ],
       type: "cards",
     },
     {
       name: "대출",
-      content: ["신한은행", "NH농협은행", "하나은행", "우리은행", "KB국민은행"],
+      content: [
+        { company: "신한은행", logo: shinhan },
+        { company: "NH농협은행", logo: nh },
+        { company: "하나은행", logo: hana },
+        { company: "우리은행", logo: woori },
+        { company: "KB국민은행", logo: kb },
+      ],
       type: "loans",
     },
     {
       name: "연금",
-      content: ["신한은행", "NH농협은행", "하나은행", "우리은행", "KB국민은행"],
+      content: [
+        { company: "신한은행", logo: shinhan },
+        { company: "NH농협은행", logo: nh },
+        { company: "하나은행", logo: hana },
+        { company: "우리은행", logo: woori },
+        { company: "KB국민은행", logo: kb },
+      ],
       type: "pensions",
     },
   ];
@@ -73,12 +105,12 @@ export default function MyDataChoose({
     const itemType = currentTab.type;
     setSelectedItemsByTab((prevSelectedItemsByTab) => {
       const newSelectedItems = prevSelectedItemsByTab[selectedTab].includes(
-        item
+        item.company
       )
         ? prevSelectedItemsByTab[selectedTab].filter(
-            (prevItem) => prevItem !== item
+            (prevItem) => prevItem !== item.company
           )
-        : [...prevSelectedItemsByTab[selectedTab], item];
+        : [...prevSelectedItemsByTab[selectedTab], item.company];
 
       const allItemsSelected = currentTab.content.every((tabItem) =>
         newSelectedItems.includes(tabItem)
@@ -103,7 +135,8 @@ export default function MyDataChoose({
 
   const handleSelectAll = () => {
     const currentTab = tabs.find((tab) => tab.name === selectedTab);
-    const currentTabContent = currentTab.content;
+    // const currentTabContent = currentTab.content;
+    const currentTabContent = currentTab.content.map((item) => item.company);
     const itemType = currentTab.type;
 
     if (allSelected[selectedTab]) {
@@ -144,9 +177,9 @@ export default function MyDataChoose({
         </p>
       </div>
       <div className="flex flex-row mt-[2vh] mb-[4vh] space-x-4 border-b-[0.2vh] border-gray-300 tabs">
-        {tabs.map((tab) => (
+        {tabs.map((tab, index) => (
           <button
-            key={tab.name}
+            key={index}
             className={`tab px-[4vw] py-2 ${
               selectedTab === tab.name
                 ? "border-b-[0.2vh] border-main font-bold"
@@ -190,13 +223,13 @@ export default function MyDataChoose({
           .find((tab) => tab.name === selectedTab)
           .content.map((item, index) => (
             <div
-              key={item}
+              key={index}
               className="flex flex-row items-center w-full h-[7vh] border-b-[0.1vh] item border-input_color mt-[1vh] gap-2"
             >
-              <img className="h-[3vh]" src={logo} alt="logo" />
+              <img className="w-[3vh] h-[3vh]" src={item.logo} alt="logo" />
 
-              <div key={item} className="w-[78vw] py-2 ">
-                {item}
+              <div key={item} className="w-[76vw] py-2 ml-3 ">
+                {item.company}
               </div>
 
               <label className="relative inline-block cursor-pointer checkbox-container">
@@ -204,7 +237,8 @@ export default function MyDataChoose({
                   type="checkbox"
                   className="absolute opacity-0 hidden-checkbox peer"
                   checked={
-                    selectedItemsByTab[selectedTab]?.includes(item) || false
+                    selectedItemsByTab[selectedTab]?.includes(item.company) ||
+                    false
                   }
                   onChange={() => handleCheckboxChange(item)}
                 />
