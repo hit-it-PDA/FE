eaa26dda0608abba70e5e081ad984f6506d3f5ab
chanjin1998 <chanjin@kookmin.ac.kr>
2024-06-22 19:14:59 +0900

    #61 feat: 카카오톡 로그인 시 유저의 자산정보 불러오는 것 추가

--- src/routes/login/KaKaoLoginPage.jsx
+++ src/routes/login/KaKaoLoginPage.jsx
@@ -2,21 +2,32 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 // apis
 import { kakaoLogin } from "../../lib/apis/userApi";
+import { getAllAssets } from "../../lib/apis/mydataApi";
 // store
 import useUserStore from "../../store/userStore";
 
 export default function KaKaoLoginPage() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
-  const { setUser } = useUserStore();
-
+  const setUser = useUserStore((state) => state.setUser);
+  const { setAsset } = useUserStore();
   let code = new URL(window.location.href).searchParams.get("code");
+
+  const fetchGetAllAssets = async () => {
+    try {
+      const response = await getAllAssets();
+      setAsset(response.response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const reqkakaoLogin = async (code) => {
     const response = await kakaoLogin(code);
     if (response.status === 200) {
       const userInfo = response.data.response.userInfo;
       setUser(userInfo);
+      fetchGetAllAssets();
       navigate("/");
     } else {
       navigate("/login");
