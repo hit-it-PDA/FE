491524013271c662a92e8952a7a12cbad92fe204
yeri <rlafl7942@naver.com>
2024-06-23 17:38:47 +0900

    #75 feat: 각 input마다 valid state 설정 후 유효성 검사

--- src/routes/manage/diagnosis/ManageDiagnosisPage.jsx
+++ src/routes/manage/diagnosis/ManageDiagnosisPage.jsx
@@ -12,14 +12,20 @@ export default function ManageDiagnosisPage() {
 
   // for first page
   const [age, setAge] = useState("");
+  const [ageValid, setAgeValid] = useState(true);
   const [effort, setEffort] = useState(-1);
   const [livingExpenses, setLivingExpenses] = useState("");
+  const [livingExpensesValid, setLivingExpensesValid] = useState(true);
 
   // for second page
   const [receive, setReceive] = useState("");
+  const [receiveValid, setReceivedValid] = useState(true);
   const [estate, setEstate] = useState("");
+  const [estateValid, setEstateValid] = useState(true);
   const [asset, setAsset] = useState("");
+  const [assetValid, setAssetValid] = useState(true);
   const [returns, setReturns] = useState("");
+  const [returnsValid, setReturnsValid] = useState(true);
 
   // result
   const result = {
@@ -32,20 +38,25 @@ export default function ManageDiagnosisPage() {
     total_real_estate_value: Number(estate),
   };
 
-  const handleChange = (e, setValue) => {
+  const handleChange = (e, setValue, setValid, below, above) => {
     const inputValue = e.target.value;
     if (/^\d*$/.test(inputValue)) {
       setValue(inputValue);
+      if (Number(inputValue) < below || Number(inputValue) > above)
+        setValid(false);
+      else setValid(true);
     } else {
-      alert("숫자만 입력해주세요!");
+      // alert("숫자만 입력해주세요!");
       setValue("");
+      setValid(false);
     }
   };
 
-  const handleBlur = (below, above, value, setValue) => {
+  const handleBlur = (below, above, value, setValue, setValid) => {
     const numericValue = Number(value);
     if (value !== "" && (numericValue < below || numericValue > above)) {
-      alert(`${below} 이상 ${above} 이하의 숫자만 입력해주세요!`);
+      // alert(`${below} 이상 ${above} 이하의 숫자만 입력해주세요!`);
+      setValid(false);
       setValue("");
     }
   };
@@ -53,10 +64,14 @@ export default function ManageDiagnosisPage() {
   const firstPageStates = {
     age,
     setAge,
+    ageValid,
+    setAgeValid,
     effort,
     setEffort,
     livingExpenses,
     setLivingExpenses,
+    livingExpensesValid,
+    setLivingExpensesValid,
     handleChange,
     handleBlur,
   };
@@ -64,12 +79,20 @@ export default function ManageDiagnosisPage() {
   const secondPageStates = {
     receive,
     setReceive,
+    receiveValid,
+    setReceivedValid,
     estate,
     setEstate,
+    estateValid,
+    setEstateValid,
     asset,
     setAsset,
+    assetValid,
+    setAssetValid,
     returns,
     setReturns,
+    returnsValid,
+    setReturnsValid,
     handleChange,
     handleBlur,
   };
