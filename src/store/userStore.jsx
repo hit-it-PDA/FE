9c43cf2edff87846e10b42ac5b6a5e6a9fb41bbd
chanjin1998 <chanjin@kookmin.ac.kr>
2024-06-23 18:58:02 +0900

    #65 feat: 기본 user 정보에 asset 추가

--- src/store/userStore.jsx
+++ src/store/userStore.jsx
@@ -4,7 +4,7 @@ import { persist } from "zustand/middleware";
 const useUserStore = create(
   persist(
     (set) => ({
-      user: { name: "방문자", type: "미정" },
+      user: { name: "방문자", type: "미정", asset: 0 },
       setUser: (user) => set({ user }),
       modifyType: (newType) =>
         set((state) => ({
@@ -20,7 +20,8 @@ const useUserStore = create(
             asset: asset,
           },
         })),
-      clearUser: () => set({ user: { name: "방문자", type: "미정" } }),
+      clearUser: () =>
+        set({ user: { name: "방문자", type: "미정", asset: 0 } }),
     }),
     {
       name: "user-storage",
