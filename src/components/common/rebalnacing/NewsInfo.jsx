62224db2e4cd4ce9a102a0f9e1e56627fe4b5876
yeri <rlafl7942@naver.com>
2024-06-24 19:38:40 +0900

    #83 feat: 주식 로고 불러오기 구현

--- src/components/common/rebalnacing/NewsInfo.jsx
+++ src/components/common/rebalnacing/NewsInfo.jsx
@@ -7,7 +7,18 @@ export default function NewsInfo() {
   return (
     <div className="flex w-full py-3 bg-white">
       <div className="flex flex-col flex-1 truncate">
-        <span className="text-[16px] font-bold">삼성전자</span>
+        <div className="flex items-center">
+          <img
+            src={
+              "https://file.alphasquare.co.kr/media/images/stock_logo/kr/005930.png"
+            }
+            className="rounded-full w-[22px]"
+          />
+          <span className="text-[16px] font-bold mx-1">삼성전자</span>
+          <span className="text-[12px] self-end text-[#525252] truncate">
+            매출: -197억, 영업이익: 1321억
+          </span>
+        </div>
         <span className="w-full text-[13px] truncate">
           “삼성 없으면 큰일날 판”…생각보다 심각한 상황이라는데“삼성 없으면
           큰일날 판”…생각보다 심각한 상황이라는데“삼성 없으면 큰일날
