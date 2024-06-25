0b7cade524ef6faecc312955493b3fb99a15282a
chanjin1998 <chanjin@kookmin.ac.kr>
2024-06-25 17:16:48 +0900

    #85 feat: 알림 읽기 api 연동

--- src/components/notification/NotificationListComponent.jsx
+++ src/components/notification/NotificationListComponent.jsx
@@ -1,16 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { patchNotification } from "../../lib/apis/notificationApi";
 
-export default function NotificationListComponent({ date, content }) {
-  const [isRead, setIsRead] = useState(false);
+export default function NotificationListComponent({
+  id,
+  date,
+  content,
+  check,
+}) {
+  const [isRead, setIsRead] = useState(check);
+  const fetchPatchNotification = async (notificationId) => {
+    try {
+      const response = await patchNotification(notificationId);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div
       className={`${
-        isRead ? "bg-white" : "bg-sub"
-      } border-b px-[8vw] py-3 h-[8vh] w-full flex items-center justify-around hover:cursor-pointer`}
-      onClick={() => setIsRead(true)}
+        isRead ? "bg-white" : "bg-[#FAFAFA] border-gray-200 border-y-[0.001px]"
+      } px-[8vw] py-3 h-[8vh] w-full flex items-center justify-between hover:cursor-pointer `}
+      onClick={() => {
+        setIsRead(true);
+        fetchPatchNotification(id);
+      }}
     >
-      <span>{date}</span>
-      <span className="flex justify-center flex-1">{content}</span>
+      <p className="text-[18px]">리밸런싱 리포트</p>
+      <div className="flex flex-col items-end">
+        <span className="flex justify-center flex-1">{content}</span>
+        <span className="text-[12px]">{date}</span>
+      </div>
     </div>
   );
 }
