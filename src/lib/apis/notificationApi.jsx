db8fafeb723843024f9345993042471bb9fd33df
youhyeoneee <khukyh2018@khu.ac.kr>
2024-06-26 00:49:59 +0900

    #93 fix: 변경된 uri 반영

--- src/lib/apis/notificationApi.jsx
+++ src/lib/apis/notificationApi.jsx
@@ -1,32 +1,35 @@
 import { authInstance } from "./base";
 
 const getNotification = async () => {
-  try {
-    const response = await authInstance(8081).get(`/notifications`);
-    return response.data;
-  } catch (error) {
-    return error.response;
-  }
+	try {
+		const response = await authInstance(8081).get(`/users/notifications`);
+		return response.data;
+	} catch (error) {
+		return error.response;
+	}
 };
 
 const postNotification = async (reqBody) => {
-  try {
-    const response = await authInstance(8084).post(`/send/message`, reqBody);
-    return response.data;
-  } catch (error) {
-    return error.response;
-  }
+	try {
+		const response = await authInstance(8084).post(
+			`/portfolios/send/message`,
+			reqBody
+		);
+		return response.data;
+	} catch (error) {
+		return error.response;
+	}
 };
 
 const patchNotification = async (notificationId) => {
-  try {
-    console.log(notificationId);
-    const response = await authInstance(8081).patch(
-      `/notifications/${notificationId}`
-    );
-    return response.data;
-  } catch (error) {
-    return error.response;
-  }
+	try {
+		console.log(notificationId);
+		const response = await authInstance(8081).patch(
+			`/users/notifications/${notificationId}`
+		);
+		return response.data;
+	} catch (error) {
+		return error.response;
+	}
 };
 export { getNotification, postNotification, patchNotification };
