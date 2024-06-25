92bbcba7a9a5cac3594dcda59ee6133aa0ba5ef9
youhyeoneee <khukyh2018@khu.ac.kr>
2024-06-25 23:09:00 +0900

    chore: localhost에서 public ip로 변경
    #93

--- src/lib/apis/portfolioApi.jsx
+++ src/lib/apis/portfolioApi.jsx
@@ -2,106 +2,106 @@ import instance, { authInstance } from "./base";
 import axios from "axios";
 
 export async function getAllPortfolio() {
-  try {
-    // const response = await authInstance(8084).get("/portfolios/hitit");
-    const response = await axios.get(
-      "http://localhost:8084/api/portfolios/hitit"
-    );
-    return response.data;
-  } catch (error) {
-    return error.response;
-  }
+	const baseUrl = import.meta.env.VITE_PORTFOLIO_BASE_URI;
+	const port = 8084;
+	const url = `${baseUrl}:${port}`;
+
+	try {
+		// const response = await authInstance(8084).get("/portfolios/hitit");
+		const response = await axios.get(`${url}/api/portfolios/hitit`);
+		return response.data;
+	} catch (error) {
+		return error.response;
+	}
 }
 
 export async function getFundList(id) {
-  try {
-    const response = await axios.get(
-      `http://localhost:8084/api/portfolios/hitit/${id}`
-    );
-    return response.data;
-  } catch (error) {
-    return error.response;
-  }
+	try {
+		const response = await axios.get(`${url}/api/portfolios/hitit/${id}`);
+		return response.data;
+	} catch (error) {
+		return error.response;
+	}
 }
 
 export async function getFundDetail(portfolioId, fundId) {
-  try {
-    const response = await axios.get(
-      `http://localhost:8084/api/portfolios/hitit/${portfolioId}/${fundId}`
-    );
-    return response.data;
-  } catch (error) {
-    return error.response;
-  }
+	try {
+		const response = await axios.get(
+			`${url}/api/portfolios/hitit/${portfolioId}/${fundId}`
+		);
+		return response.data;
+	} catch (error) {
+		return error.response;
+	}
 }
 
 export async function getMyDataPortfolio(body) {
-  const token = localStorage.getItem("accessToken");
-  try {
-    const response = await axios.post(
-      `http://3.39.217.69:8084/api/portfolios/mydata/leveltest`,
-      body
-      // {
-      //   headers: {
-      //     Authorization: `Bearer ${token}`,
-      //   },
-      // }
-    );
-    return response.data;
-  } catch (error) {
-    return error.response;
-  }
+	const token = localStorage.getItem("accessToken");
+	try {
+		const response = await axios.post(
+			`${url}/api/portfolios/mydata/leveltest`,
+			body
+			// {
+			//   headers: {
+			//     Authorization: `Bearer ${token}`,
+			//   },
+			// }
+		);
+		return response.data;
+	} catch (error) {
+		return error.response;
+	}
 }
 
 export async function changePortfolio(id) {
-  try {
-    const response = await authInstance(8084).post(`/portfolios/change/${id}`);
-    return response.data;
-  } catch (error) {
-    return error.response;
-  }
+	try {
+		const response = await authInstance(8084).post(`/portfolios/change/${id}`);
+		return response.data;
+	} catch (error) {
+		return error.response;
+	}
 }
 
 export async function getUserPortfolio() {
-  try {
-    const response = await authInstance(8084).get(`/portfolios/user`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    return error.response;
-  }
+	try {
+		const response = await authInstance(8084).get(`/portfolios/user`);
+		return response.data;
+	} catch (error) {
+		console.log(error);
+		return error.response;
+	}
 }
 
 export async function changeMyDataPortfolio(body) {
-  try {
-    const response = await authInstance(8084).post(
-      `/portfolios/mydata/change`,
-      body
-    );
-    return response.data;
-  } catch (error) {
-    return error.response;
-  }
+	try {
+		const response = await authInstance(8084).post(
+			`/portfolios/mydata/change`,
+			body
+		);
+		return response.data;
+	} catch (error) {
+		return error.response;
+	}
 }
 
 export async function getUserPortfolioDetail() {
-  try {
-    const response = await authInstance(8084).get(`/portfolios/userfunds`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    return error.response;
-  }
+	try {
+		const response = await authInstance(8084).get(`/portfolios/userfunds`);
+		return response.data;
+	} catch (error) {
+		console.log(error);
+		return error.response;
+	}
 }
-    
+
 export async function getUserFundDetail(fundId) {
-  try {
-    const response = await authInstance(8084).get(
-      `/portfolios/userfunds/detail/${fundId}`
-    );
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    return error.response;
-  }
+	try {
+		const response = await authInstance(8084).get(
+			`/portfolios/userfunds/detail/${fundId}`
+		);
+		return response.data;
+	} catch (error) {
+		console.log(error);
+		return error.response;
+	}
 }
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8081",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+export default defineConfig(({ mode }) => {
+	// Load environment variables based on the current mode
+	const env = loadEnv(mode, process.cwd());
+
+	return {
+		plugins: [react()],
+		server: {
+			proxy: {
+				"/api": {
+					target: env.VITE_USER_BASE_URI + ":8081",
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, ""),
+				},
+			},
+		},
+	};
 });
