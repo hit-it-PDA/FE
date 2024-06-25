c9bfe6fd15c311feb5d6b5e13e560340c8afe37b
yeri <rlafl7942@naver.com>
2024-06-26 00:40:06 +0900

    #95 feat: 현재 수익률 비회원일 경우 처리

--- src/routes/manage/ManagePage.jsx
+++ src/routes/manage/ManagePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 // components
 import LineChartComponent from "../../components/common/chart/LineChartComponent";
@@ -6,15 +7,48 @@ import TopBar from "../../components/common/topBar/TopBar";
 import ManageTitleComponent from "../../components/manage/ManageTitleComponent";
 import MoreServiceComponent from "../../components/manage/MoreServiceComponent";
 
+// store
+import useUserStore from "../../store/userStore";
+
 export default function ManagePage() {
+  const navigate = useNavigate();
+  const user = useUserStore((store) => store.user);
   return (
     <div>
       <TopBar type={0} />
       <div className="flex flex-col w-full h-full gap-10 p-5">
-        <ManageTitleComponent stockReturns="1,000" />
-        <LineChartComponent
-          returnsData={[200, 100, 382, 423, 12, 452, 322, 85, 33, 77, 23, 199]}
-        />
+        <span className="text-[23px] font-bold">📌 현재 수익률</span>
+        {user?.name === "방문자" ? (
+          <div className="relative w-full h-full">
+            <div className="absolute z-20 flex flex-col text-[20px] items-center justify-center w-full h-full">
+              <span className="p-2 font-bold">로그인 후 이용 가능합니다.</span>
+              <button
+                className="bg-main px-10 py-2 rounded-[20px] mt-5 text-[15px] text-white font-bold"
+                onClick={() => navigate("/login")}
+              >
+                로그인
+              </button>
+            </div>
+            <div className="w-full h-full blur-sm">
+              <ManageTitleComponent stockReturns="???" />
+              <LineChartComponent
+                returnsData={[
+                  200, 100, 382, 423, 12, 452, 322, 85, 33, 77, 23, 199,
+                ]}
+              />
+            </div>
+          </div>
+        ) : (
+          <>
+            <ManageTitleComponent stockReturns="1,000" />
+            <LineChartComponent
+              returnsData={[
+                200, 100, 382, 423, 12, 452, 322, 85, 33, 77, 23, 199,
+              ]}
+            />
+          </>
+        )}
+
         <div className="w-full h-[30vh] flex flex-col">
           <span className="text-[23px] font-bold">📌 부가 서비스</span>
           <div className="flex flex-col justify-around flex-1 mt-2">
